f0f4b7fde689ffb8d349579256f1dd989aeee3fa
Nathan <nathanvanengelen@gmail.com>
2018-05-09 13:34:36 +0200

    Trajectory naar klassediagram

--- KBS_TZT/src/me/ictm2j/tzt/Trajectory.java
+++ KBS_TZT/src/me/ictm2j/tzt/Trajectory.java
@@ -2,4 +2,25 @@ package me.ictm2j.tzt;
 
 public class Trajectory extends Route {
 
+	private int trajectory_id;
+	private int distance;
+	private double cost;
+	
+	public Trajectory(int trajectory_id, int distance, double cost) {
+		this.trajectory_id = trajectory_id;
+		this.distance = distance;
+		this.cost = cost;
+		
+	}
+	
+	public int getTrajectory_id() {
+		return this.trajectory_id;
+	}
+	public int getDistance() {
+		return this.distance;
+	}
+	public double getCost() {
+		return this.cost;
+	}
+	
 }
