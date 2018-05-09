9988202a936a4432a79482d31bbc2a27e960de31
Nathan <nathanvanengelen@gmail.com>
2018-05-09 19:05:44 +0200

    Bike courier Courier en Delivery naar Klasseschema gemaakt

--- KBS_TZT/src/me/ictm2j/tzt/Bikecourier.java
+++ KBS_TZT/src/me/ictm2j/tzt/Bikecourier.java
@@ -2,7 +2,27 @@ package me.ictm2j.tzt;
 
 public class Bikecourier implements Courier {
 
-	public Bikecourier () {
-		super();
+	
+	private int courier_id;
+	private String name;
+	private String route;
+	
+	public Bikecourier (int courier_id, String name, String route) {
+		this.courier_id = courier_id;
+		this.name = name;
+		this.route = route;
+	}
+	
+	@Override
+	public int getCourier_id() {
+		return this.courier_id;
+	}
+	@Override
+	public String getName() {
+		return this.name;
+	}
+	@Override
+	public String getRoute() {
+		return this.route;
 	}
 }
--- KBS_TZT/src/me/ictm2j/tzt/Courier.java
+++ KBS_TZT/src/me/ictm2j/tzt/Courier.java
@@ -2,4 +2,7 @@ package me.ictm2j.tzt;
 
 public interface Courier {
 
+	String getName();
+	int getCourier_id();
+	String getRoute();
 }
