623238b1d3c06d0c38b8ce4deef8fcb0b4be9125
Nathan <nathanvanengelen@gmail.com>
2018-05-09 13:13:31 +0200

    package naar klassemodel

--- KBS_TZT/src/me/ictm2j/tzt/Package.java
+++ KBS_TZT/src/me/ictm2j/tzt/Package.java
@@ -2,4 +2,39 @@ package me.ictm2j.tzt;
 
 public class Package {
 
+	
+	private int height;
+	private int width;
+	private int length;
+	private int weight;
+	private int pakketnummer;
+	
+	
+
+
+	public Package(int height, int width, int length, int weight, int pakketnummer ) {
+		
+		this.height = height;
+		this.width = width;
+		this.length = length;
+		this.weight = weight;
+		this.pakketnummer = pakketnummer;
+	}
+		public int getHeight() {
+			return this.height;
+		}
+		public int getWidth() {
+			return this.width;
+		}
+		public int getLength() {
+			return this.length;
+		}
+		public int getWeight() {
+			return this.weight;
+		}
+		public int getPakketnummer() {
+			return this.pakketnummer;
+		}
+	
+	
 }
