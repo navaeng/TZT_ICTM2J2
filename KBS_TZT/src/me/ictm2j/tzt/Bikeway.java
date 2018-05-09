36f384d6d02bc0589f6a6c58457866fc6a45e2df
Kelvin Boerkamp <info@kelvinboerkamp.nl>
2018-05-09 12:44:21 +0200

    bikeway ingevuld via klassendiagram

--- KBS_TZT/src/me/ictm2j/tzt/Bikeway.java
+++ KBS_TZT/src/me/ictm2j/tzt/Bikeway.java
@@ -1,5 +1,22 @@
 package me.ictm2j.tzt;
 
 public class Bikeway extends Route{
+	private double distance;
+	private int costs;
+	
+	public Bikeway(double distance, int costs){
+		this.distance = distance;
+		this.costs = costs;
+	}
 
+	public double getDistance() {
+		return distance;
+	}
+
+	public int getCosts() {
+		return costs;
+	}
+
+	
+	
 }
