763c4caab56f4875094a96d7e18ff6d1941e5c03
Kelvin Boerkamp <info@kelvinboerkamp.nl>
2018-05-17 14:46:13 +0200

    json parser

--- KBS_TZT/src/me/ictm2j/tzt/GoogleDirectionsAPIConnection.java
+++ KBS_TZT/src/me/ictm2j/tzt/GoogleDirectionsAPIConnection.java
@@ -1,6 +1,37 @@
 package me.ictm2j.tzt;
 
+
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.*;
+import com.sun.deploy.net.URLEncoder;
+
 public class GoogleDirectionsAPIConnection {
-	import org.
+
+private void directions(String city, String city2) throws Exception{
+	String s = "https://maps.googleapis.com/maps/api/directions/json?origin=";
+	s += URLEncoder.encode(city, "UTF-8") + "&destination=";
+	s += URLEncoder.encode(city2, "UTF-8") + "&key=AIzaSyDy-AawwBez3tmTzKrypv90_xMkxwhXrvY&language=nl&mode=transit&transit_mode=train";
+	URL url = new URL(s);
+	System.out.println(url);
+	
+	
+	// read from the url
+	Scanner scan = new Scanner (url.openStream());
+	String str = new String();
+	while (scan.hasNext()) {
+		str += scan.nextLine();
+	}
+	scan.close();
+
+	// buld json object
+	JSONObject obj = new JSONObject(str);
+	if (!obj.getString("status").equals("OK")) {
+		return;
+	}
+	
+	
+	}
 
 }
