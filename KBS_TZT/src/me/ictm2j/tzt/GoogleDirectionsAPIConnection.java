d08121070c09b02002a77fdb794845693a3afc43
Kelvin Boerkamp <info@kelvinboerkamp.nl>
2018-05-18 10:22:43 +0200

    eerste resultaat ophalen vanuit api

--- KBS_TZT/src/me/ictm2j/tzt/GoogleDirectionsAPIConnection.java
+++ KBS_TZT/src/me/ictm2j/tzt/GoogleDirectionsAPIConnection.java
@@ -1,15 +1,16 @@
 package me.ictm2j.tzt;
 
-
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Scanner;
 
+
 import org.json.*;
-import com.sun.deploy.net.URLEncoder;
 
 public class GoogleDirectionsAPIConnection {
 
-private void directions(String city, String city2) throws Exception{
+
+public static void directions(String city, String city2) throws Exception{
 	String s = "https://maps.googleapis.com/maps/api/directions/json?origin=";
 	s += URLEncoder.encode(city, "UTF-8") + "&destination=";
 	s += URLEncoder.encode(city2, "UTF-8") + "&key=AIzaSyDy-AawwBez3tmTzKrypv90_xMkxwhXrvY&language=nl&mode=transit&transit_mode=train";
@@ -32,6 +33,51 @@ private void directions(String city, String city2) throws Exception{
 	}
 	
 	
+	// get the first result
+	
+	String sa = "";
+	for( int i = 0; i < obj.getJSONArray("routes").length(); i++){
+		JSONObject r1 = (JSONObject) obj.getJSONArray("routes").get(i);
+		System.out.println(r1.getString("copyrights"));
+	
+
+	
+		for (int x = 0; x < r1.getJSONArray("legs").length(); x++) {
+			JSONObject o1 = ((JSONObject) r1.getJSONArray("legs").get(x));
+			System.out.println(o1.getString("start_address"));
+			sa = o1.getString("start_address");
+			System.out.println(o1.getString("end_address"));
+		
+			
+			for (int j = 0; j < o1.getJSONArray("steps").length(); j++ ) {
+				JSONObject step1 = ((JSONObject) o1.getJSONArray("steps").get(j));
+				System.out.println(step1.getString("travel_mode"));
+				System.out.println(step1.getJSONObject("distance").getString("text"));
+				System.out.println(step1.getJSONObject("duration").getString("text"));
+			}
+	
 	}
+
+}
+}
+
+public static void main(String[] args) {
+	try {
+		System.out.println("Wat is het vertrekpunt ");
+		@SuppressWarnings("resource")
+		Scanner scanner1 = new Scanner(System.in);
+		String beginplaats = scanner1.nextLine();
+		
+		System.out.println("Wat is de eindbestemming ");
+		@SuppressWarnings("resource")
+		Scanner scanner2 = new Scanner(System.in);
+		String eindbestemming = scanner2.nextLine();
+		
+		GoogleDirectionsAPIConnection.directions(beginplaats, eindbestemming);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
 
 }
