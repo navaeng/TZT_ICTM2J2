8cc17103e2821f6cacaa552a53e44b79338abe45
shadowhunter1423 <peter.greive@Live.nl>
2018-05-19 15:51:34 +0200

    Keygen file

---
+++ KBS_TZT/src/me/ictm2j/tzt/TZTcourierKeygen.java
@@ -0,0 +1,76 @@
+package me.ictm2j.tzt;
+
+import javax.swing.JPanel;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.protocol.Resultset;
+import java.awt.Rectangle;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+
+public class TZTcourierKeygen extends JPanel {
+	
+	public static void main(String[] args) {
+		Connection.openConnection();
+		TZTcourierDashboard frame = new TZTcourierDashboard();
+		frame.setVisible(false);
+		System.out.println("Ik ben main");
+		new TZTcourierKeygen();
+		try {
+			PreparedStatement key = Connection.connection.prepareStatement("SELECT generatedKey FROM Keygen WHERE keygenID = 1");
+			key.executeQuery();
+			
+			ResultSet result = key.getResultSet();
+			
+			result.next();
+			
+			System.out.println(result.getInt("generatedKey"));
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("oeps");
+			e1.printStackTrace();
+		}
+	}
+	
+	private final JLabel lblNewLabel = new JLabel("New label");
+
+	/**
+	 * Create the panel.
+	 */
+	public TZTcourierKeygen() {
+		setBackground(Color.LIGHT_GRAY);
+		setBounds(new Rectangle(10, 11, 452, 291));
+		setLayout(null);
+		
+		JButton btnGenereerKey = new JButton("Genereer Key");
+		btnGenereerKey.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					PreparedStatement key = Connection.connection.prepareStatement("SELECT generatedKey FROM Keygen WHERE keygenID = 1");
+					System.out.println(key);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					System.out.println("oeps");
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		btnGenereerKey.setBounds(147, 93, 149, 52);
+		add(btnGenereerKey);
+		lblNewLabel.setBounds(157, 158, 126, 36);
+		add(lblNewLabel);
+		this.setVisible(true);
+
+	}
+}
--- KBS_TZT/src/me/ictm2j/tzt/Testing.java
+++ KBS_TZT/src/me/ictm2j/tzt/Testing.java
@@ -1,5 +1,15 @@
 package me.ictm2j.tzt;
 //main
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.protocol.Resultset;
+
 public class Testing {
 
 	public static void main(String[] args) {
