e8f6425fb4186537ab7a99778dc86d373d7834ef
Nathan <nathanvanengelen@gmail.com>
2018-05-22 15:08:42 +0200

    admin

---
+++ KBS_TZT/src/me/ictm2j/tzt/AdminAddDelivery.java
@@ -0,0 +1,130 @@
+package me.ictm2j.tzt;
+
+import javax.swing.JPanel;
+import java.awt.Rectangle;
+import javax.swing.JList;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import java.awt.Color;
+
+public class AdminAddDelivery extends JPanel implements ActionListener{
+	private JTextField textFieldDeliveryID;
+	private JTextField textFieldUserID;
+	private JTextField textFieldLockerID;
+	private JTextField textFieldRouteID;
+	private JTextField textFieldDatum;
+	private JTextField textFieldpackage;
+	private JPanel	   AdminAddDeliver;
+
+	/**
+	 * Create the panel.
+	 */
+	public AdminAddDelivery() {
+		setBounds(new Rectangle(10, 1, 452, 291));
+		setLayout(null);
+		
+		JLabel jlabel = new JLabel("tekst");
+		jlabel.setBounds(10, 43, 172, 212);
+		jlabel.setBackground(Color.WHITE);
+		add(jlabel);
+		
+		JLabel lblLeveringSamenstellen = new JLabel("Levering samenstellen");
+		lblLeveringSamenstellen.setBounds(192, 7, 135, 25);
+		lblLeveringSamenstellen.setFont(new Font("Arial", Font.BOLD, 11));
+		add(lblLeveringSamenstellen);
+		
+		JLabel lblDeliveryId = new JLabel("Delivery ID");
+		lblDeliveryId.setBounds(192, 43, 62, 25);
+		add(lblDeliveryId);
+		
+		JLabel lblUserId = new JLabel("User ID");
+		lblUserId.setBounds(192, 79, 62, 25);
+		add(lblUserId);
+		
+		JLabel lblLockerId = new JLabel("Locker ID");
+		lblLockerId.setBounds(192, 115, 62, 25);
+		add(lblLockerId);
+		
+		JLabel lblRouteId = new JLabel("Route ID");
+		lblRouteId.setBounds(192, 151, 62, 25);
+		add(lblRouteId);
+		
+		JLabel lblDatumId = new JLabel("Datum");
+		lblDatumId.setBounds(192, 187, 62, 25);
+		add(lblDatumId);
+		
+		textFieldDeliveryID = new JTextField();
+		textFieldDeliveryID.setBounds(264, 45, 86, 20);
+		add(textFieldDeliveryID);
+		textFieldDeliveryID.setColumns(10);
+		
+		textFieldUserID = new JTextField();
+		textFieldUserID.setBounds(264, 81, 86, 20);
+		textFieldUserID.setColumns(10);
+		add(textFieldUserID);
+		
+		textFieldLockerID = new JTextField();
+		textFieldLockerID.setBounds(264, 117, 86, 20);
+		textFieldLockerID.setColumns(10);
+		add(textFieldLockerID);
+		
+		textFieldRouteID = new JTextField();
+		textFieldRouteID.setBounds(264, 153, 86, 20);
+		textFieldRouteID.setColumns(10);
+		add(textFieldRouteID);
+		
+		textFieldDatum = new JTextField();
+		textFieldDatum.setBounds(264, 189, 86, 20);
+		textFieldDatum.setColumns(10);
+		add(textFieldDatum);
+		
+		JButton buttonSamenstellen = new JButton("Samenstellen");
+		buttonSamenstellen.setBounds(192, 255, 158, 25);
+		buttonSamenstellen.addActionListener(this);
+		add(buttonSamenstellen);
+		
+		JLabel lblRouteInformatie = new JLabel("Route informatie");
+		lblRouteInformatie.setBounds(10, 12, 135, 25);
+		lblRouteInformatie.setFont(new Font("Arial", Font.BOLD, 11));
+		add(lblRouteInformatie);
+		
+		JLabel labelpackage = new JLabel("Datum");
+		labelpackage.setBounds(192, 219, 62, 25);
+		add(labelpackage);
+		
+		textFieldpackage = new JTextField();
+		textFieldpackage.setBounds(264, 220, 86, 20);
+		textFieldpackage.setColumns(10);
+		add(textFieldpackage);
+		
+		JButton btnOphalen = new JButton("Ophalen");
+		btnOphalen.setBounds(10, 256, 172, 25);
+		add(btnOphalen);
+
+	}
+	public void actionPerformed(ActionEvent e) {
+		try {
+			String strd=textFieldDeliveryID.getText();
+			String stru=textFieldUserID.getText();
+			String strl=textFieldLockerID.getText();
+			String strda=textFieldDatum.getText();
+								
+			Statement s=Connection.connection.createStatement();
+			
+				
+				s.execute("INSERT INTO Package (deliveryID,userID,packageID,lockerID,routeID,date)VALUES('"+ strd + "' , '"+ stru + "', '" + strl + "','" + strda +"')");
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}	
+	}
+}
---
+++ KBS_TZT/src/me/ictm2j/tzt/AdminAddRoute.java
@@ -0,0 +1,120 @@
+package me.ictm2j.tzt;
+
+import javax.swing.JPanel;
+import java.awt.Rectangle;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+
+public class AdminAddRoute extends JPanel implements ActionListener{
+	private static final String strba = null;
+	private JTextField textFieldBeginadres;
+	private JTextField textFieldEindadres;
+	private JTextField textFieldBeginStation;
+	private JTextField textFieldEindStation;
+	private JPanel AdminAddRoute;
+	private JPanel AdminAddDelivery;
+	public String steers;
+	public String sters;
+
+
+	/**
+	 * Create the panel.
+	 */
+	public AdminAddRoute() {
+		setBounds(new Rectangle(10, 11, 452, 291));
+		setLayout(null);
+		
+		JLabel lblLeveringRoute = new JLabel("Levering route");
+		lblLeveringRoute.setFont(new Font("Arial", Font.BOLD, 11));
+		lblLeveringRoute.setBounds(10, 11, 123, 25);
+		add(lblLeveringRoute);
+		
+		JLabel lblBeginpunt = new JLabel("Begin adres");
+		lblBeginpunt.setBounds(10, 59, 123, 25);
+		add(lblBeginpunt);
+		
+		JLabel lblBeginStation = new JLabel("Begin station");
+		lblBeginStation.setBounds(10, 95, 123, 25);
+		add(lblBeginStation);
+		
+		JLabel lblEindStation = new JLabel("Eind station");
+		lblEindStation.setBounds(10, 131, 123, 25);
+		add(lblEindStation);
+		
+		JLabel lblAfleverAdres = new JLabel("Aflever adres");
+		lblAfleverAdres.setBounds(10, 167, 123, 25);
+		add(lblAfleverAdres);
+		
+		textFieldBeginadres = new JTextField();
+		textFieldBeginadres.setColumns(10);
+		textFieldBeginadres.setBounds(187, 59, 205, 25);
+		add(textFieldBeginadres);
+		
+		textFieldBeginStation = new JTextField();
+		textFieldBeginStation.setColumns(10);
+		textFieldBeginStation.setBounds(187, 97, 205, 25);
+		add(textFieldBeginStation);
+		
+		textFieldEindStation = new JTextField();
+		textFieldEindStation.setColumns(10);
+		textFieldEindStation.setBounds(187, 133, 205, 25);
+		add(textFieldEindStation);
+
+		textFieldEindadres = new JTextField();
+		textFieldEindadres.setColumns(10);
+		textFieldEindadres.setBounds(187, 167, 205, 25);
+		add(textFieldEindadres);
+		
+		JButton btnNewButtonRouteToevoegen = new JButton("Toevoegen");
+		btnNewButtonRouteToevoegen.addActionListener(this);
+		btnNewButtonRouteToevoegen.setBounds(187, 203, 205, 31);
+		add(btnNewButtonRouteToevoegen);
+		
+		
+		
+	    }
+	public  void actionPerformed(ActionEvent e) {
+		try {
+			
+			String strba = textFieldBeginadres.getText();
+			String strbs=textFieldBeginStation.getText();
+			String stres=textFieldEindStation.getText();
+			String strea=textFieldEindadres.getText();
+			this.steers = strba;
+			this.sters = strbs;
+			
+			
+								
+			Statement s=Connection.connection.createStatement();
+			
+				
+				s.execute("INSERT INTO Route (startAdress,startStation,endStation,endAdress)VALUES('"+ strba + "' , '"+ strbs + "', '" + stres + "','" + strea +"')");
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}	
+		
+		
+		AdminAddRoute.setVisible(false);
+		AdminAddDelivery.setVisible(true);
+	}
+	private String setBeginAdres(String steers) {
+		return this.steers;
+			}
+	private String setBeginStation(String sters) {
+		return this.sters;
+			}
+	private String setEindStation(String stres) {
+		return this.sters;
+			}
+	private String setEindAdres(String strea) {
+		return this.strea;
+			}
+	
+}
